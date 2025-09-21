8b8eaaba8dfca6601e0b105c3299f261f97f20e2
kojiro <kojirotec@gmail.com>
2025-09-21 22:39:21 +0900

    formsをModelFromに変更

--- typeApp/forms.py
+++ typeApp/forms.py
@@ -1,12 +1,9 @@
 from django import forms
 from django.forms import ModelForm, inlineformset_factory
+from .models import Transcription
 
-
-class TranscriptionForm(forms.Form):
-    # 'text'という名前の入力欄を定義します。
-    # CharFieldは文字列の入力欄です。
-    text = forms.CharField(
-        label='文字起こし', 
-        widget=forms.Textarea(attrs={'rows': 20, 'cols': 80}) # Textareaウィジェットを使い、入力欄を大きくします。
-    )
+class TranscriptionForm(ModelForm):
+    class Meta:
+        model = Transcription
+        fields = ['text']
 
--- typeApp/models.py
+++ typeApp/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+
+class Transcription(models.Model):
+    text = models.TextField(verbose_name="入力欄")
